9d1967595c6e0adbd128ac5369dda9936739ac5d
RNGPuns <47067008+RNGPuns@users.noreply.github.com>
2020-11-04 17:11:34 -0500

    Fenêtres de l'interface et icônes

--- src/application/InterfaceGraphique.java
+++ src/application/InterfaceGraphique.java
@@ -5,17 +5,37 @@ import donnees.Catalogue;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class InterfaceGraphique extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		Group root = new Group();
-		
+		TabPane root = new TabPane();
+		VBox vbox = new VBox(root);
 		Catalogue catalogue = Catalogue.getInstance();
 		
-		Scene scene = new Scene(root,500,450);
+		Tab tabCatalogue = new Tab("Catalogue");
+        Tab tabLivres = new Tab("Livres");
+        Tab tabDVD = new Tab("DVD");
+        Tab tabPeriodique = new Tab("Périodiques");
+        
+        tabCatalogue.setGraphic(new ImageView("icon-collection.png"));
+        tabLivres.setGraphic(new ImageView("icon-livre.png"));
+        tabDVD.setGraphic(new ImageView("icon-dvd.png"));
+        tabPeriodique.setGraphic(new ImageView("icon-periodique.png"));
+
+        root.getTabs().add(tabCatalogue);
+        root.getTabs().add(tabLivres);
+        root.getTabs().add(tabDVD);
+        root.getTabs().add(tabPeriodique);
+        
+		Scene scene = new Scene(vbox,500,450);
 		arg0.setTitle("Médiathèque");
 		arg0.setScene(scene);
 		arg0.show();
--- src/application/Main.java
+++ src/application/Main.java
@@ -2,9 +2,9 @@ package application;
 	
 import javafx.application.Application;
 
-
+//Démarre l'application
 public class Main {
 	public static void main(String[] args) {
-		Application.launch(InterfaceGraphique.class, args); //Démarrage de l'application
+		Application.launch(InterfaceGraphique.class, args); 
 	}
 }
